35b07a19f0f0002205490e79d5acf57900156165
Naveen Guttedar <naveenguttedar844@gmail.com>
2024-02-13 18:57:24 +0530

    added README.md and updated script.js(user addTask)

---
+++ Naveen/README.md
@@ -0,0 +1,42 @@
+# Trello Project
+**Project Homepage**: [Trello Project](https://haripriyaa1224.github.io/Team9-Project/Naveen/)
+This project aims to create a simplified version of Trello, a web-based list-making application.
+
+## Table of Contents
+
+- [Introduction](#introduction)
+- [Features](#features)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Introduction
+
+Trello Project is a web application designed to help users organize tasks and projects using a simple and intuitive interface. Inspired by Trello's board and card system, this project allows users to create boards, lists, and cards to manage their tasks efficiently.
+
+## Features
+
+- **Create Tasks**: Users can create Tasks to represent different projects or categories.
+- **Add Lists**: Within each board, users can add lists to organize tasks based on different stages or categories.
+- **Create Cards**: Users can create cards within lists to represent individual tasks or items.
+- **Drag and Drop**: Cards can be moved between lists using a drag-and-drop interface, allowing users to track progress easily.
+- **Local Storage**: This cards and board stored in local storage so that user data remains same evern after reloading the page ,closing the browser etc.
+
+## Installation
+
+To run the Trello Project locally, follow these steps:
+
+1. Clone the Folder to your local machine.
+git clone https://github.com/Haripriyaa1224/Team9-Project.git
+2. Navigate to the Naveen directory.
+
+## Contributing
+
+Contributions to the Trello Project are welcome! To contribute, follow these steps:
+
+1. Fork the repository.
+2. Create a new branch for your feature or bug fix.
+3. Make your changes and commit them with descriptive messages.
+4. Push your changes to your fork.
+5. Submit a pull request to the main repository.
--- Naveen/script.js
+++ Naveen/script.js
@@ -1,29 +1,29 @@
 console.log("Hello World");
 const task_list = document.getElementById('task_list');
 const on_going_task = document.getElementById("on_going_tasks")
-const completed_tasks=document.getElementById("completed_tasks")
+const completed_tasks = document.getElementById("completed_tasks")
 const on_going_task_list = document.getElementById('on_going_task_list');
 const completed_task_list = document.getElementById('completed_task_list');
 const favDialog = document.getElementById('favDialog');
 const add_task_btn = document.getElementById('add_task');
-const submit_btn=document.getElementById('btn_submit');
+const submit_btn = document.getElementById('btn_submit');
 console.log(completed_tasks);
 let dumb_text = "Some quick example text to build on the card title and make up the bulk of the card's content."
 let task_data = localStorage.getItem("task_data");
 let on_going_task_data = localStorage.getItem("on_going_task_data");
-let completed_task_data=localStorage.getItem("completed_task_data");
+let completed_task_data = localStorage.getItem("completed_task_data");
 if (task_data == null) {
   task_data = [{ card_title: "📚 Genre Exploration", card_text: " Read a chapter of a book from a genre you don't typically explore 📖." }, { card_title: "🧘 Mindful Moments", card_text: "Practice meditation for 15 minutes 🧘." }, { card_title: " 💬 Connection Quest", card_text: "Reach out to a friend or family member you haven't spoken to in a while and catch up 🗣️." }]
   localStorage.setItem("task_data", JSON.stringify(task_data));
 }
 task_data = JSON.parse(localStorage.getItem("task_data"));
-if (on_going_task_data == null){
-  on_going_task_data = [{ card_title: "🏋️ Sweat Session", card_text: "Do a 30-minute workout session 💪." },{ card_title: "🎧 Podcast Pursuit:", card_text: "Listen to a podcast episode on a topic you're curious about 🎧." }];
+if (on_going_task_data == null) {
+  on_going_task_data = [{ card_title: "🏋️ Sweat Session", card_text: "Do a 30-minute workout session 💪." }, { card_title: "🎧 Podcast Pursuit:", card_text: "Listen to a podcast episode on a topic you're curious about 🎧." }];
   localStorage.setItem("on_going_task_data", JSON.stringify(on_going_task_data));
 }
 on_going_task_data = JSON.parse(localStorage.getItem("on_going_task_data"))
-if(completed_task_data==null){
-completed_task_data = [{ card_title: "📝 Poetic Waves:", card_text:" Write a poem about the ocean" },{ card_title: "🍳 Culinary Adventure", card_text:"Try cooking a new recipe you've never attempted before." }];
+if (completed_task_data == null) {
+  completed_task_data = [{ card_title: "📝 Poetic Waves:", card_text: " Write a poem about the ocean" }, { card_title: "🍳 Culinary Adventure", card_text: "Try cooking a new recipe you've never attempted before." }];
   localStorage.setItem("completed_task_data", JSON.stringify(completed_task_data));
 }
 
@@ -31,9 +31,9 @@ completed_task_data = JSON.parse(localStorage.getItem("completed_task_data"));
 const makeCard = (card_type, card_idx, data) => {
   const card = document.createElement('div');
   card.setAttribute("draggable", "true")
-  card.setAttribute("id",card_type+"-"+card_idx);
+  card.setAttribute("id", card_type + "-" + card_idx);
   card.classList.add('card');
-  const cardId=card_type=="task"?1:card_type=="on_going"?2:3;
+  const cardId = card_type == "task" ? 1 : card_type == "on_going" ? 2 : 3;
   card.innerHTML = `<div class="card_top_sec">
               <p class="card_title">${data.card_title}</p>
               <div class="dropdown">
@@ -52,12 +52,12 @@ const makeCard = (card_type, card_idx, data) => {
               <p>${data.card_text}</p>
             </div>`
 
-  card.addEventListener("dragstart", (e) => { 
+  card.addEventListener("dragstart", (e) => {
     card.classList.add('draggable');
-    const cardObj={ card_type, card_idx,node:JSON.stringify(card)};
+    const cardObj = { card_type, card_idx, node: JSON.stringify(card) };
     e.dataTransfer.setData("text/plain", JSON.stringify(cardObj));
   })
-  card.addEventListener('dragend',()=>{
+  card.addEventListener('dragend', () => {
     card.classList.remove('draggable');
   })
   return card;
@@ -74,42 +74,42 @@ on_going_task_data.map((e, i) => {
 
 completed_task_data.map((e, i) => {
   const card = makeCard("completed", i, completed_task_data[i]);
-    completed_task_list.appendChild(card);
+  completed_task_list.appendChild(card);
 })
-const addToLocalStorage=(key,value)=>{
-    localStorage.setItem(key, JSON.stringify(value))
+const addToLocalStorage = (key, value) => {
+  localStorage.setItem(key, JSON.stringify(value))
 }
 function deletCard(card_type_id, card_idx) {
-  const card_type=card_type_id==1?"task":card_type_id==2?"on_going":"completed"
-    let element = document.getElementById(card_type+"-" + card_idx);
+  const card_type = card_type_id == 1 ? "task" : card_type_id == 2 ? "on_going" : "completed"
+  let element = document.getElementById(card_type + "-" + card_idx);
   if (card_type_id == 1) {
     element.remove();
     task_data.splice(card_idx, 1);
-    addToLocalStorage("task_data",task_data)
+    addToLocalStorage("task_data", task_data)
   }
-  else if(card_type_id==2){
+  else if (card_type_id == 2) {
     element.remove();
-    on_going_task_data.splice(card_idx,1);
-    addToLocalStorage("on_going_task_data",on_going_task_data)
+    on_going_task_data.splice(card_idx, 1);
+    addToLocalStorage("on_going_task_data", on_going_task_data)
   }
-  else{
+  else {
     element.remove();
-    completed_task_data.splice(card_idx,1);
-    addToLocalStorage("completed_task_data",completed_task_data)
+    completed_task_data.splice(card_idx, 1);
+    addToLocalStorage("completed_task_data", completed_task_data)
   }
 }
 add_task_btn.addEventListener('click', () => {
-  addTask("newBi","Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.")
+  favDialog.showModal();
 })
-submit_btn.addEventListener('click',()=>{
-  const cardTitle=document.getElementById('card-title').value;
-  const cardText=document.getElementById('card-text').value;
-  addTask(cardTitle,cardText);
+submit_btn.addEventListener('click', () => {
+  const cardTitle = document.getElementById('card-title').value;
+  const cardText = document.getElementById('card-text').value;
+  addTask(cardTitle, cardText);
 })
-const addTask=(cardTitle,cardText)=>{
-  task_data.push({card_title:cardTitle,card_text:cardText});
-  addToLocalStorage("task_data",task_data);
-  const card = makeCard("task", task_data.length-1,task_data[task_data.length-1]);
+const addTask = (cardTitle, cardText) => {
+  task_data.push({ card_title: cardTitle, card_text: cardText });
+  addToLocalStorage("task_data", task_data);
+  const card = makeCard("task", task_data.length - 1, task_data[task_data.length - 1]);
   task_list.appendChild(card);
   favDialog.showModal();
 }
@@ -127,41 +127,41 @@ function onDragleave(e) {
 function onDragover(e) {
   e.preventDefault();
 }
-function onDrop(e,container_type, container_list) {
+function onDrop(e, container_type, container_list) {
   let cardObj = e.dataTransfer.getData("text");
   cardObj = JSON.parse(cardObj);
-  let card_type=cardObj.card_type,card_idx=cardObj.card_idx;
-  if(container_type==card_type){
+  let card_type = cardObj.card_type, card_idx = cardObj.card_idx;
+  if (container_type == card_type) {
     console.log("this are same types");
     return
   }
-  const card_data=card_type=="task"?{id:1,list:task_data}:card_type=="on_going"?{id:2,list:on_going_task_data}:{id:3,list:completed_task_data};
-  if(container_type=="on_going"){
+  const card_data = card_type == "task" ? { id: 1, list: task_data } : card_type == "on_going" ? { id: 2, list: on_going_task_data } : { id: 3, list: completed_task_data };
+  if (container_type == "on_going") {
     on_going_task_data.push(card_data.list[card_idx]);
-  addToLocalStorage("on_going_task_data",on_going_task_data)
+    addToLocalStorage("on_going_task_data", on_going_task_data)
     let len = on_going_task_data.length - 1;
     const card = makeCard("on_going", len, on_going_task_data[len]);
-    deletCard(card_data.id,card_idx);
-   container_list.appendChild(card);
+    deletCard(card_data.id, card_idx);
+    container_list.appendChild(card);
   }
-  else{
+  else {
     completed_task_data.push(card_data.list[card_idx]);
-    let len = completed_task_data.length - 1 ; addToLocalStorage("completed_task_data",completed_task_data);
+    let len = completed_task_data.length - 1; addToLocalStorage("completed_task_data", completed_task_data);
     const card = makeCard("completed", len, completed_task_data[len]);
-    deletCard(card_data.id,card_idx);
+    deletCard(card_data.id, card_idx);
     container_list.appendChild(card);
   }
   e.target.classList.remove("dragover");
   e.preventDefault();
 }
-const dragHandler=(contianer,container_type,contianer_list)=>{
-contianer.addEventListener('dragover', (e) => onDragover(e ))
-contianer.addEventListener('dragenter', (e) => onDragenter(e))
-contianer.addEventListener('dragleave', (e) => onDragleave(e))
-contianer.addEventListener('drop', (e) => onDrop(e,container_type, contianer_list));
+const dragHandler = (contianer, container_type, contianer_list) => {
+  contianer.addEventListener('dragover', (e) => onDragover(e))
+  contianer.addEventListener('dragenter', (e) => onDragenter(e))
+  contianer.addEventListener('dragleave', (e) => onDragleave(e))
+  contianer.addEventListener('drop', (e) => onDrop(e, container_type, contianer_list));
 }
-dragHandler(on_going_task,"on_going",on_going_task_list);
-dragHandler(completed_tasks,"completed",completed_task_list);
+dragHandler(on_going_task, "on_going", on_going_task_list);
+dragHandler(completed_tasks, "completed", completed_task_list);
 
 
 
